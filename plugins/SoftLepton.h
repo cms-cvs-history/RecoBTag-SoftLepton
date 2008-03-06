339ca553605259abbde0e94a757b959c0f19b9db
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2008-03-06 14:28:56 +0000

    backport SoftLepton from 2.0.0-pre4

--- plugins/SoftLepton.h
+++ plugins/SoftLepton.h
@@ -16,7 +16,7 @@
 //
 // Original Author:  fwyzard
 //         Created:  Wed Oct 18 18:02:07 CEST 2006
-// $Id: SoftLepton.h,v 1.2 2007/10/08 16:16:47 fwyzard Exp $
+// $Id: SoftLepton.h,v 1.3 2008/03/03 10:52:27 fwyzard Exp $
 //
 
 // system include files
@@ -65,6 +65,11 @@ protected:
       const math::XYZVector& axis
   );
 
+  static double boostedPPar(
+      const math::XYZVector& vector,
+      const math::XYZVector& axis
+  );
+  
 private:
   virtual void beginJob(const edm::EventSetup& iSetup);
   virtual void produce(edm::Event& iEvent, const edm::EventSetup& iSetup);
