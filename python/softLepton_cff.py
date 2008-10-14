55a2293edf9c81d4dd8d0558e2e8e15fd430f79e
cmsbuild <>
2008-10-14 16:45:45 +0000

    This commit was manufactured by cvs2git to create tag 'RecoBTag- SoftLepton-V04-05-01'.
    Sprout from master 2008-08-27 11:15:58 UTC Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch> 'move TransientTrackBuilder access from beginJob to produce, as it is record-dependant'
    Cherrypick from master 2008-10-14 16:45:44 UTC Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch> 'ad missing ES thingy':
        python/softLepton_cff.py
        python/softMuonByIP3dBJetTags_cfi.py
    Cherrypick from master 2008-10-14 09:38:44 UTC Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch> 'add softMuonByPtBJetTags (default, positive, negative)':
        python/softMuonByPtBJetTags_cfi.py

--- python/softLepton_cff.py
+++ python/softLepton_cff.py
@@ -12,4 +12,7 @@ from RecoBTag.SoftLepton.softMuonES_cfi import *
 from RecoBTag.SoftLepton.softMuonBJetTags_cfi import *
 from RecoBTag.SoftLepton.softMuonNoIPES_cfi import *
 from RecoBTag.SoftLepton.softMuonNoIPBJetTags_cfi import *
-
+from RecoBTag.SoftLepton.softLeptonByPtES_cfi import *
+from RecoBTag.SoftLepton.softMuonByPtBJetTags_cfi import *
+from RecoBTag.SoftLepton.softLeptonByIP3dES_cfi import *
+from RecoBTag.SoftLepton.softMuonByIP3dBJetTags_cfi import *
